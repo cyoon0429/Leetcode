b63c76b641ceb1590d55f4b187737fb105e331c2
cyoon0429 <60642021+cyoon0429@users.noreply.github.com>
2021-03-11 23:31:07 -0800

    First commit

--- Move Zeroes.java
+++ Move Zeroes.java
@@ -1,28 +1,21 @@
 /*
-Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 
-
-
 Example 1:
-
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 
-
 Example 2:
-
 Input: nums = [0]
 Output: [0]
  
-
 Constraints:
-
 1 <= nums.length <= 104
 -231 <= nums[i] <= 231 - 1
  
-
 Follow up: Could you minimize the total number of operations done?
 
 HINT 1:
@@ -43,27 +36,23 @@ on the non-zero elements of the array.
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int countZero = 0;
-        int p = 0;
+        int countZero = 0; //count the number of zeros in array
+        int p = 0; // pointer
         
         for (int i = 0; i < nums.length; i++ ){
                                
             if (nums[i] == 0){
                 countZero++;  
 
-                
-            }else  {
-                
+            } else  {
                 nums[p] = nums[i];
                 p++;
-
-                
             }
         }
         
         for (int i = nums.length-countZero; i<nums.length; i++){
             nums[i] = 0;
         }
-        
     }
 }
+//asdfasdf
